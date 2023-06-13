6fdbcbbfc3c30ab8263a161dd71204006ff86c43
William Du <wdu@rbi.com>
2023-06-13 16:11:08 -0400

    added swift files to implement Live Activities natively in ios

---
+++ ios/liveActivity-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  liveActivity-Bridging-Header.h
+//  reactnativedemo
+//
+//  Created by William Du on 6/13/23.
+//
+
+#ifndef liveActivity_Bridging_Header_h
+#define liveActivity_Bridging_Header_h
+
+
+#endif /* liveActivity_Bridging_Header_h */
---
+++ ios/liveActivity.swift
@@ -0,0 +1,9 @@
+//
+//  liveActivity.swift
+//  reactnativedemo
+//
+//  Created by William Du on 6/13/23.
+//
+
+import Foundation
+
